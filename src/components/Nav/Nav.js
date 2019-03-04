262f578784e0279e5c9452ac7e7986a30a76debe
unknown <pp_nancy@hotmail.com>
2019-03-03 20:26:07 -0500

    saved

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,48 @@
-import React from "react";
-//import NavMessage from "../NavMessage";
+import React, { Components} from "react";
 import "./Nav.css";
 
+class Nav extends Components {
+  componentsWillUnmont(){
+    window.clearTimeout(this.timeout)
+  }
+  renderMessage( winGame, correct, clear = false) {
+    let navMessage, className
+    if(clear) {
+      className =''
+    }
+    else if (correct === undefined){
+      navMessage ='Play again => Click an image'
+      className =''
+    } else {
+      navMessage = winGame ? ' Congratulations! You won the game!':(correct ? ' Wow! correctly guessed': 'Opp. sorry! you had been already clicked that image!')
+      className = correct ? ' correct': 'incorrect'
 
-const Nav = props => (
-  <nav className="navbar navbar-dark bg-primary">
-  <a herf ="/">Clicky Game </a>
-   {/* < NavMessage score={props.score} topScore ={props.topScore} />*/}
-  <h2>Score: {props.score} | Top Score:{props.topScore} </h2>
-  </nav>
-);
+    }
+    window.clearTimeout(this.timeout)
+    if(!clear & correct !== undefined){
+      this.timeout = window.setTimeout
+      (this.renderMessage, winGame ? 3000 : 1000, winGame ? undefined : correct, false, true)
+    }
+    return <li key={Math.random()} className ={className}>{navMessage}</li>
+  }
+  render(){
+    return(
+      <nav className='pin'>
+        <div>
+          <ul className = 'center navList'>
+            <li className = 'name'> Clicky Game:A memory game practitioner </li>
+            {this.renderMessage
+            (this.props.correct,
+              this.props.winGame)}
+              <li> Srore: {this.props.score} | Hight Score: {this.props.highScore}</li>
+            </ul>
+        </div>
+      
+      </nav>
+    )
+  }
+}
+
+
 
 export default Nav;
