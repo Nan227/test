4b90095c4c9fb08f6a6b1ed916808d47f68f3c3e
unknown <pp_nancy@hotmail.com>
2019-03-04 02:29:59 -0500

    saved

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer =()=> (
+  <footer classNAme='footer bg-info'>
+    <div className="container">
+    <span className=' text-dark'> Clicky Game : Test your memory!</span></div>
+  </footer>
+)
+export default Footer;
